5f3ca0e19b8e6a838c1a584bec5e01710f3a12cf
Larry Adigun <larryadigun.student@careerdevs.com>
2018-12-14 21:10:46 +0000

    update

--- array.js
+++
@@ -1,9 +0,0 @@
-//Multiplying the length of arrays
-
-var colors = ["pink", "red", "black", "purple"];
-var rooms = ["room1", "room2", "room3", "room4"];
-
-var combos = colors.length * rooms.length;
-
-console.log(combos);
-
