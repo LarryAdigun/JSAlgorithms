014a0273dcb850a6bdae89b9036522162791bb73
Larry Adigun <larryadigun.student@careerdevs.com>
2019-01-28 16:28:49 +0000

    random

--- tallest.js
+++ tallest.js
@@ -1,7 +1,7 @@
 //Max and min of an array you have to use apply
 
 var tallest = [121.92, 124.46,  127.00, 132.08 ,134.62, 137.16, 139.70 , 149.86, 154.94, 157.48];
-var taller = Math.min.apply(null, tallest)
+var taller = Math.min.apply(null, tallest);
 console.log(taller);
 
 
